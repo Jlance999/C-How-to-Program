df4b02c3de70f88b2a52f147733d21cf86152068
Jlance <Jlance@users.noreply.github.com>
2019-10-26 13:31:20 -0400

    Update Arithmetic Functions.c

--- Arithmetic Functions.c
+++ Arithmetic Functions.c
@@ -2,6 +2,7 @@
 
 int main( void) {
 
+									// Initializing global variables.
 int int1;
 int int2;
 int sum;
@@ -10,14 +11,14 @@ int quotient;
 int product;
 int remainder;
 
-printf("Enter first integer: ");
-scanf("%d", &int1);
+printf("Enter first integer: ");	// Prompts user for integer value
+scanf("%d", &int1);					// Records integer value to be used in calculations
 
 printf("Enter second integer: ");
 scanf("%d", &int2);
 
-sum = int1 + int2;
-printf("\n\nSum: %d.\n", sum);
+sum = int1 + int2;					// Stores the sum of the 2 integer variables as a sum variable.
+printf("\n\nSum: %d.\n", sum);		// Prints Sum: and the value stored in the sum variable.
 
 difference = int1 - int2;
 printf("Difference: %d.\n", difference);
@@ -28,5 +29,5 @@ printf("Quotient: %d.\n", quotient);
 remainder = int1 % int2;
 printf("Remainder: %d.\n", remainder);
 
-return 0;
+return 0;							// End program (non commented functions are self explanatory.)
 }
