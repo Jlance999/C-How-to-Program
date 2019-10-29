a4b283c9440e030a39b4fdf20ae8aee80745f8d7
Jlance999 <jeremy.perkins99@gmail.com>
2019-10-29 19:57:40 -0400

    Create Body Mass Index Calculator.c

---
+++ Body Mass Index Calculator.c
@@ -0,0 +1,21 @@
+#include <stdio.h>  // Included to allow for printf/scanf
+
+int main(void) {
+  float weight;     // Initializing variables
+  float height;
+  float bmi;
+
+  printf("Please enter weight in pounds: ");    // Prompting user for input
+  scanf("%f", &weight);                         // Storing user input to variable
+
+  printf("Please enter height in inches: ");
+  scanf("%f", &height);
+
+  bmi = (weight*703) / (height * height);       // Performing calculations using user input 
+
+// Formatted BMI Value chart for comparison with calculation.
+  printf("\n\nBMI Values\nUnderweight:\tless than 18.5\nNormal:\t\t\tBetween 18.5 and 24.9\nOverweight:\t\tbetween 25 and 29.9\nObese:\t\t\t30 or greater.\n\n");
+// Displaying calculated BMI
+  printf("Your bmi is %.2f\n", bmi);
+  return 0; // End
+}
