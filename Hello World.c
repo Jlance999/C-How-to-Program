d4cc3a521d9017efce42a4739b6483dfdd279994
Jlance999 <jeremy.perkins99@gmail.com>
2019-10-31 19:18:58 -0400

    Huge Repo Restructure
    Initilized each program as an .exe and organized all programs into their own folders based on the chapter in which they were introduced.

--- Hello World.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h> // Include standard input and output library functions (such as printf).
-
-int main(void) {
-  // Main function, every program must have one. Void means it receives no values.
-  printf("Hello World\n"); // What will be displayed, f stands for "formatted", \n begins a new line after World
-  return 0; // Every line must end with a ;, functions are defined between a set of braces. 0 is the value returned by this function as we do not need it to return a value.
-}
