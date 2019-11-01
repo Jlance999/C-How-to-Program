043b6e96ce4e5e5bfe12db2ab3acd6eb63b4d6f3
Jlance999 <jeremy.perkins99@gmail.com>
2019-10-31 20:05:49 -0400

    Commented last program.

--- Chapter III/Printing Numbers from a Loop/Printing Numbers from a Loop.c
+++ Chapter III/Printing Numbers from a Loop/Printing Numbers from a Loop.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 
+/*
+Write a program that utilizes looping to print the numbers from 1 to 10 side by side on the same line with three spaces between numbers.
+*/
+
 int main(void) {
 
-  int x = 1;
-  
-  while (x<=10) {
-    printf("%d   ",x);
-	
-	x++;
+  int x = 1;            // Initializing Global Variable
+
+  while (x<=10) {       // Loop will end when X is 11 or greater
+    printf("%d   ",x);  // Prints value of x
+
+	x++;                // Increments value of x+1
   }
-  return 0;
+  return 0;             // End
 }
