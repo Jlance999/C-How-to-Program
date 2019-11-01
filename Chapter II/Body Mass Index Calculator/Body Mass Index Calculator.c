7a235940996a0d5d22194dd489208f7e67d05b96
Jlance999 <jeremy.perkins99@gmail.com>
2019-10-31 20:52:02 -0400

    Added getchar for usability outside of commandline

--- Chapter II/Body Mass Index Calculator/Body Mass Index Calculator.c
+++ Chapter II/Body Mass Index Calculator/Body Mass Index Calculator.c
@@ -11,11 +11,18 @@ int main(void) {
   printf("Please enter height in inches: ");
   scanf("%f", &height);
 
-  bmi = (weight*703) / (height * height);       // Performing calculations using user input 
+  bmi = (weight*703) / (height * height);       // Performing calculations using user input
 
 // Formatted BMI Value chart for comparison with calculation.
   printf("\n\nBMI Values\nUnderweight:\tless than 18.5\nNormal:\t\t\tBetween 18.5 and 24.9\nOverweight:\t\tbetween 25 and 29.9\nObese:\t\t\t30 or greater.\n\n");
 // Displaying calculated BMI
   printf("Your bmi is %.2f\n", bmi);
+
+  printf("\nPress ENTER key to exit program.\n");
+
+// To pause end of execution until a key is pressed
+  getchar();
+  getchar();
+
   return 0; // End
 }
